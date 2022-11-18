a47a7ba932a424c6c0b2f801facbf1ef3e716388
Cesar Li (win10) <cesarli619@gmail.com>
2022-11-17 23:13:03 -0500

    Added comments to KillScript

--- KillPartScript.lua
+++ KillPartScript.lua
@@ -1,5 +1,8 @@
 local killPart = script.Parent
 
+-- Kill function requires a parameter 'otherPart'
+-- This parameter will be given to us by
+--the .Touched event in the last line.
 local function kill(otherPart)
 	local partParent = otherPart.Parent
 	local human = partParent:FindFirstChild("Humanoid")
@@ -9,4 +12,7 @@ local function kill(otherPart)
 	end
 end
 
+-- This line sends information about whatever
+--entity (part, character, etc.) touched our 
+--kill part.
 killPart.Touched:Connect(kill)
